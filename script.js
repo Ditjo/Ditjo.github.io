3fdcbfa3595900d271fa78b76dcd50566d25638f
Ditjo <97234116+Ditjo@users.noreply.github.com>
2022-08-10 14:54:04 +0200

    V0.1 of new site layout

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//open menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+//Close menu
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
