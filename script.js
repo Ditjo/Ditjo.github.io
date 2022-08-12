5349fa8f115aef3c03c5afb5cb4af87b1d3edc86
Ditjo <97234116+Ditjo@users.noreply.github.com>
2022-08-12 12:59:08 +0200

    V0.1.2

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+  function switchLanguage() {
+    document.getElementById("swLa").src="Images/LA_EN_IMG.png"
+    /*if (document.getElementById("swLa").src == "Images/LA_DK_IMG.png"){
+    document.getElementById("swLa").src="Images/LA_EN_IMG.png"  
+    } else {
+      document.getElementById("swLa").src="Images/LA_DK_IMG.png" 
+    }*/
+    
+  }
