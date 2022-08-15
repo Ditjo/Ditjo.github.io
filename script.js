cec246b5620cc99ea50685e7db76c4383917ed09
Ditjo <mads933c@elev.tec.dk>
2022-08-15 18:06:34 +0200

    Text file added, language changer works after test

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+//on load
+  window.onload = function(){
+    lang_dk ();
+  }
+
+
 //open menu
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -8,13 +14,24 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-
+  //Switch Language
   function switchLanguage() {
-    document.getElementById("swLa").src="Images/LA_EN_IMG.png"
-    /*if (document.getElementById("swLa").src == "Images/LA_DK_IMG.png"){
+    if (document.getElementById("swLa").src == "http://127.0.0.1:5500/Images/LA_DK_IMG.png"){
     document.getElementById("swLa").src="Images/LA_EN_IMG.png"  
     } else {
       document.getElementById("swLa").src="Images/LA_DK_IMG.png" 
-    }*/
+    }
     
   }
+
+  function lang_dk(){
+    
+      document.getElementById("welcome_txt").innerHTML = welcome_dk;
+  }
+
+  function lang_en(){
+
+  }
+
+  var welcome_dk = "Jeg har haft en interesse for computere siden jeg var barn."
+
