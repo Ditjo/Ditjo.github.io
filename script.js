f9be64059e0ba81585e44b0978b824a27992930a
Ditjo <97234116+Ditjo@users.noreply.github.com>
2022-08-16 14:54:35 +0200

    language switch set on hold

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 //on load
-  window.onload = function(){
-    lang_dk ();
-  }
+// var lang = "dk";
+//   window.onload = function(){
+//   if (lang == "dk")
+//   {
+//     lang_dk();
+//   }
+//   else
+//   {
+//     lang_en();
+//   } 
+
+//   }
+
 
 
 //open menu
@@ -14,24 +24,39 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  //Switch Language
-  function switchLanguage() {
-    if (document.getElementById("swLa").src == "http://127.0.0.1:5500/Images/LA_DK_IMG.png"){
+  //Switch Language Visual
+ /* 
+ function switchLanguage() {
+    if (document.getElementById("swLa").src == "http://127.0.0.1:5500/Images/LA_DK_IMG.png")
+    {
     document.getElementById("swLa").src="Images/LA_EN_IMG.png"  
-    } else {
-      document.getElementById("swLa").src="Images/LA_DK_IMG.png" 
+    lang = "en";
+    lang_en();
+    } 
+    else 
+    {
+      document.getElementById("swLa").src="Images/LA_DK_IMG.png"
+      lang = "dk";
+      lang_dk(); 
     }
-    
   }
 
   function lang_dk(){
-    
       document.getElementById("welcome_txt").innerHTML = welcome_dk;
   }
 
   function lang_en(){
-
+      document.getElementById("welcome_txt").innerHTML = welcome_en;
   }
 
-  var welcome_dk = "Jeg har haft en interesse for computere siden jeg var barn."
+  function readText(){
 
+  }
+  var welcome_dk = "Jeg har haft en interesse for computere siden jeg var barn. Det startede primært med bare at spille spil, men senere begyndte jeg også at blive Tech-support " +
+      "for min familie. Efter et år på HTX skiftede jeg retning og startede som elektriker. Denne uddannelse gjorde jeg færdig, " +
+      "men jeg vidste at det ikke var det jeg skulle bruge resten af mit liv på. Jeg tog en tur til New Zealand i et halvt års tid og kom hjem, " +
+      "og fortsatte med at arbejde som elektriker indtil en af mine venner præsenteret mig for Datatekniker uddannelsen. " +
+      "Jeg tog nogle enkeltfag på VUC før jeg begyndte på Datatekniker uddannelsen."
+
+  var welcome_en = "I have always had an interest in computres ever since i was a child."
+  */
